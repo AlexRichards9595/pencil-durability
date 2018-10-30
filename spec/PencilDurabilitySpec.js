78c2ab4a33e4e2e161b698a28d5f0e881577df40
Alex Richards <arichards@netjets.com>
2018-10-30 18:00:44 -0400

    Refactored DRY

--- spec/PencilDurabilitySpec.js
+++ spec/PencilDurabilitySpec.js
@@ -1,30 +1,33 @@
-describe("Pencil", function() {
+describe("A Pencil and a piece of paper", function(){
   var pencil = new Pencil();
   var paper = new Paper();
-
-  it("should write what you tell it to write", function(){
-    var actual = pencil.write("Knowledge comes, ", paper);
-    expect(actual).toBe("Knowledge comes, ");
-  });
-  it("should have the sharpness you ask for", function() {
-    let superSharpPencil = new Pencil(100);
-    expect(superSharpPencil.sharpness).toEqual(100);
-  });
-});
-
-
-describe("Paper", function(){
-  var pencil = new Pencil();
-  var paper = new Paper();
-  it('should not lose the next written on it from before', function(){
-    pencil.write("Knowledge comes, ", paper);
-    var actual = pencil.write("but Wisdom lingers.", paper);
-    expect(actual).toBe("Knowledge comes, but Wisdom lingers.");
-  });
-  it("should reset the paper when you scrapIt", function(){
-    pencil.write("something wrong", paper);
+  beforeEach(function(){
     paper.scrapIt();
-    var actual = pencil.write("something... write haha", paper);
-    expect(actual).toBe("something... write haha");
+  });
+
+  describe("Pencil", function() {
+    it("should write what you tell it to write", function(){
+      var actual = pencil.write("Knowledge comes, ", paper);
+      expect(actual).toBe("Knowledge comes, ");
+    });
+    it("should have the sharpness you ask for", function() {
+      let superSharpPencil = new Pencil(100);
+      expect(superSharpPencil.sharpness).toEqual(100);
+    });
+  });
+
+
+  describe("Paper", function(){
+    it('should not lose the next written on it from before', function(){
+      pencil.write("Knowledge comes, ", paper);
+      var actual = pencil.write("but Wisdom lingers.", paper);
+      expect(actual).toBe("Knowledge comes, but Wisdom lingers.");
+    });
+    it("should reset the paper when you scrapIt", function(){
+      pencil.write("something wrong", paper);
+      paper.scrapIt();
+      var actual = pencil.write("something... write haha", paper);
+      expect(actual).toBe("something... write haha");
+    });
   });
 });
